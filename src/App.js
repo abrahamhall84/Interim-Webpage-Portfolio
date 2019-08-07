2ec200f22886e5a0f20488f42cfaa2eea62819e8
abrahamhall84 <abrahamhall84@gmail.com>
2019-08-07 14:13:25 -0700

    aligned content & linked all links

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import './App.css';
+import PDF from './components/resume/AbrahamHall_DEVresumeL.pdf'
 
 function App() {
   return (
-    <div>
-      <h2 className='intro-sentence'>Hi! I'm <u className='my-name'>Abraham Hall</u></h2>
-      <span className='link-box bubble'>
-        <div className='git-link'><a>Git</a></div>
-        <div className='linkedIn-link'><a>LinkedIn</a></div>
-      </span>
+    <div className='my-app'>
+      <div className='intro-name'>
+        <h2 className='intro-sentence'>Hi! I'm <u className='my-name'>Abraham Hall</u></h2>
+        <span className='link-box bubble' hidden>
+          <div className='git-link'><a href='https://github.com/abrahamhall84' target='_blank'>Git</a></div>
+          <div className='linkedIn-link'><a href='https://www.linkedin.com/in/abrahamhall84/' target='_blank'>LinkedIn</a></div>
+        </span>
+      </div>
+
       <h3 className='job-title'>A front end developer-designer</h3>
       <p className='under-contruction'>Please pardon the construction while my magical fairies (fingers) are making my new site pretty for you.</p>
       <div className='fairy-dust'></div>
 
-
-      <h4 className='project-title'>Projects</h4>
-      <div><a>Project 1</a></div>
-      <div><a>Project 2</a></div>
-      <div><a>Project 3</a></div>
+      <div className='projects-box'>
+        <h4 className='project-title'>Projects</h4>
+        <div><a href='http://ahtimepiece.netlify.com/' target='_blank'>Project 1</a></div>
+        <div><a href='http://ahtictactoe.netlify.com/' target='_blank'>Project 2</a></div>
+        <div><a href='http://eatingrightforyourbloodtype.netlify.com/' target='_blank'>Project 3</a></div>
+      </div>
       
-      
-       
+      <h4><a href={PDF} target='_blank'>CV/ Resume</a></h4>
       
 
       { /*<img />
